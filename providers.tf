d113274bd9f00c9ede5b95cee86ec4d60acc839e
Navaneeth Rao <32010211+navaneeth-dev@users.noreply.github.com>
2024-12-24 10:50:13 +0530

    feat: rclone encrypted config, script download

--- providers.tf
+++ providers.tf
@@ -34,13 +34,21 @@ resource "aws_instance" "mirror_sync" {
   }
 
   instance_type = "c5.large"
+
+  root_block_device {
+    volume_size = 32
+  }
+
   user_data = <<EOF
 #cloud-config
+write_files:
+- content: RCLONE_ENCRYPT_V0: t2fEKG8JdfBc/788/MSmbl+CYcXRU42YvHAS5xlop+87kBEkcfiAay93M4ypi1Pvtft/LREzbSRfN58tXOwKgJbzZ+U7SLluxvZAv8whqQ5tN6mX14wWG+dQk7JARwYNbYUZ3/xd1ztkMmiCeN6Npz1qrxRb5YqQD2XSzCL7uIY6/WdEKpv6rWS+bdvPeo3MP2pcNsr9Ug5yD/X/dcgg4bs3eopVWzJcOGjRfLdN0F/wytXLdCFW5u4ixKgRmPfUQg2vZUN2lMbFi/If355hiTxx4Ax9kvNyE9QHlivUPh6HfCkVwUw+RFFxvsWG3DQ/ulY=
+  path: /root/.config/rclone/rclone.conf
 runcmd:
-- id
 - sudo mkdir /mnt/idrive
 - sudo -v ; curl https://rclone.org/install.sh | sudo bash
-- rclone mount --vfs-cache-mode full --vfs-cache-max-size 10G --transfers 16 idrive:mirror /mnt/idrive
-- wget "https://gitlab.archlinux.org/archlinux/infrastructure/-/raw/master/roles/syncrepo/files/syncrepo-template.sh" -O /usr/bin/syncrepo-template
+- rclone mount --daemon --vfs-cache-mode full --vfs-cache-max-size 24G --transfers 16 idrive:mirror /mnt/idrive
+- wget "https://github.com/navaneeth-dev/mirror-sync/syncrepo-template.sh" -O /usr/bin/syncrepo-template
+- chmod +x /usr/bin/syncrepo-template
 EOF
 }
