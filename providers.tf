d860a18b2d4e545e30d24ee608e4eed45b39b8a4
Navaneeth Rao <32010211+navaneeth-dev@users.noreply.github.com>
2024-12-29 21:18:44 +0530

    feat: initial linode provider

--- providers.tf
+++ providers.tf
@@ -1,8 +1,7 @@
 terraform {
   required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
+    linode = {
+      source = "linode/linode"
     }
   }
 
@@ -16,20 +15,8 @@ terraform {
   }
 }
 
-variable "AWS_ACCESS_KEY_ID" {
-  type = string
-}
-
-variable "AWS_SECRET_ACCESS_KEY" {
-  type = string
-}
-
 variable "RCLONE_CONFIG_PASS" {
   type = string
 }
 
-provider "aws" {
-  region     = "ap-southeast-1"
-  access_key = var.AWS_ACCESS_KEY_ID
-  secret_key = var.AWS_SECRET_ACCESS_KEY
-}
+provider "linode" {}
