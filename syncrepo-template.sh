5122e915d82fc5c9aa6f99b65e354792a09a2ff9
Navaneeth Rao <32010211+navaneeth-dev@users.noreply.github.com>
2024-12-30 13:42:36 +0530

    feat: change arch sync script

--- syncrepo-template.sh
+++ syncrepo-template.sh
@@ -63,13 +63,7 @@ find "${target}" -name '.~tmp~' -exec rm -r {} +
 
 rsync_cmd() {
 	local -a cmd=(rsync -rptH -L --safe-links --delete-delay --delay-updates
-		"--timeout=600" "--contimeout=60" --no-motd -v --info=progress2)
-
-	if stty &>/dev/null; then
-		cmd+=(-h -v --progress)
-	else
-		cmd+=(--quiet)
-	fi
+		"--timeout=600" "--contimeout=60" --no-motd -v --progress)
 
 	if ((bwlimit>0)); then
 		cmd+=("--bwlimit=$bwlimit")
